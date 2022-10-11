ddf23ecce9381c34f49d0542e339ba2ee9a37da9
yamanoi-shu <s.yamanoi00@gmail.com>
2022-10-11 22:24:30 +0900

    add: ast walk

--- gocc/gocc.go
+++ gocc/gocc.go
@@ -1,6 +1,8 @@
-package gocc
+package main
 
 import (
+	"fmt"
+	"go/ast"
 	"go/parser"
 	"go/token"
 	"log"
@@ -17,7 +19,20 @@ import (
 21 - 40    | 複雑なコードでテストが難しくなる
 --------------------------------------------
 40 -       | テスト不可能
+--------------------------------------------
 */
+
+type Report struct {
+	Pos struct {
+		Line int
+		Col  int
+	}
+	FuncName string
+	CC       int
+}
+
+var fset *token.FileSet
+var reports []Report
 
 func main() {
 	if len(os.Args) != 2 {
@@ -26,10 +41,73 @@ func main() {
 
 	filePath := os.Args[1]
 
-	fset := token.NewFileSet()
+	fset = token.NewFileSet()
 	f, err := parser.ParseFile(fset, filePath, nil, 0)
 	if err != nil {
 		log.Fatal("Failed to parse file")
 	}
 
+	reports = make([]Report, 0, fset.Position(f.Pos()).Line)
+
+	walkAll(f)
+
+	for _, v := range reports {
+		if v.CC > 10 {
+			fmt.Println(v)
+		}
+	}
+}
+
+func walkAll(root ast.Node) {
+	ast.Inspect(root, func(n ast.Node) bool {
+		switch n.(type) {
+		case *ast.FuncDecl:
+			walkFunc(n.(*ast.FuncDecl))
+		}
+		return true
+	})
+}
+
+func walkFunc(root *ast.FuncDecl) {
+	position := fset.Position(root.Pos())
+	report := Report{
+		Pos: struct {
+			Line int
+			Col  int
+		}{
+			Line: position.Line,
+			Col:  position.Column,
+		},
+		FuncName: root.Name.Name,
+		CC:       1,
+	}
+	fmt.Println(report)
+
+	ast.Inspect(root, func(n ast.Node) bool {
+		switch n.(type) {
+		case *ast.FuncDecl:
+			return false
+		case *ast.IfStmt:
+			report.CC++
+			if n.(*ast.IfStmt).Else != nil {
+				report.CC++
+			}
+		case *ast.SwitchStmt:
+			report.CC++
+		case *ast.TypeSwitchStmt:
+			report.CC++
+		case *ast.CaseClause:
+			report.CC++
+		case *ast.SelectStmt:
+			report.CC++
+		case *ast.CommClause:
+			report.CC++
+		case *ast.ForStmt:
+			report.CC++
+		case *ast.RangeStmt:
+			report.CC++
+		}
+		return true
+	})
+	reports = append(reports, report)
 }
---
+++ gocc/test.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func test() {
+	x := 1 + 1
+
+	switch x {
+	case 1:
+		fmt.Println("a")
+	case 2:
+		fmt.Println("a")
+	case 3:
+		fmt.Println("a")
+	case 4:
+		fmt.Println("a")
+	case 5:
+		fmt.Println("a")
+	case 6:
+		fmt.Println("a")
+	case 7:
+		fmt.Println("a")
+	case 8:
+		fmt.Println("a")
+	case 9:
+		fmt.Println("a")
+	case 10:
+		fmt.Println("a")
+	case 11:
+		fmt.Println("a")
+	}
+}
